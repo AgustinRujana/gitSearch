722052b32952425d78e59f7355d914dd5aefbd62
AgustinRujana <87715028+Loesen-AgustinRujana@users.noreply.github.com>
2022-02-08 19:40:35 +0100

    Update SearchBar.js

--- components/search/components/SearchBar.js
+++ components/search/components/SearchBar.js
@@ -10,7 +10,7 @@ const SearchBar = () => {
         />
         <label
           htmlFor="user_search"
-          className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-purple-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          className="absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-4 peer-focus:text-purple-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
         >
          Cerca utente
         </label>
